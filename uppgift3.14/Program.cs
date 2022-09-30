c0034b75cbebb8fc3afccac21a4f57e46c0fb137
Bergqvist1 <hibba04@hotmail.se>
2022-09-30 10:21:34 +0200

    Add project files.

---
+++ uppgift3.14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_3_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnaaiet, svara j för Ja och n för Nej");
+            string rättSvar = Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?"); 
+            int ålder = int.Parse(Console.ReadLine());
+
+            if(ålder <= 22 & rättSvar=="j" )
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+
+            else 
+            {
+                Console.WriteLine("tyvärr vi vill inte anställa dig");
+            }
+ 
+        }
+    }
+}
